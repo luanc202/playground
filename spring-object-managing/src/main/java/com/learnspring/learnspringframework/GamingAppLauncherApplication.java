1b9c010c7bff372ff3ec38567cc35ffc5ab70fbc
Luan <barkon32@tuta.io>
2023-09-30 12:54:08 -0300

    renamed main class

--- spring-object-managing/src/main/java/com/learnspring/learnspringframework/GamingAppLauncherApplication.java
+++ spring-object-managing/src/main/java/com/learnspring/learnspringframework/GamingAppLauncherApplication.java
@@ -2,15 +2,14 @@ package com.learnspring.learnspringframework;
 
 import com.learnspring.learnspringframework.game.GameRunner;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("com.learnspring.learnspringframework")
-public class App03GameSpringBeans {
+public class GamingAppLauncherApplication {
     public static void main(String[] args) {
-        try (var context = new AnnotationConfigApplicationContext(App03GameSpringBeans.class);) {
+        try (var context = new AnnotationConfigApplicationContext(GamingAppLauncherApplication.class);) {
             context.getBean(GamingConsole.class).up();
             context.getBean(GameRunner.class).run();
         }
