25e25d3360b31c6346d19d76ed5024da449a5bd3
Luan <barkon32@tuta.io>
2023-09-30 12:44:46 -0300

    moved GamingConfiguration to App03

--- spring-object-managing/src/main/java/com/learnspring/learnspringboot/App03GameSpringBeans.java
+++ spring-object-managing/src/main/java/com/learnspring/learnspringboot/App03GameSpringBeans.java
@@ -1,7 +1,24 @@
 package com.learnspring.learnspringboot;
 
 import com.learnspring.learnspringboot.game.GameRunner;
+import com.learnspring.learnspringboot.game.PacmanGame;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class GamingConfiguration {
+    @Bean
+    public GamingConsole game() {
+
+        return new PacmanGame();
+    }
+
+    @Bean
+    public GameRunner gameRunner(GamingConsole game) {
+        return new GameRunner(game);
+    }
+}
 
 public class App03GameSpringBeans {
 
